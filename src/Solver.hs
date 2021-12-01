e092b3fe78efff1939256ff6f96b76a0f1ebe841
Yanis Guaye <yanis.guaye@epitech.eu>
2021-12-01 10:12:42 +0100

    Day 01

--- src/Solver.hs
+++ src/Solver.hs
@@ -6,12 +6,15 @@ import Prelude hiding (lines)
 
 class Solver solver where
   setup :: Text -> solver
-  part1 :: solver -> Word64
-  part2 :: solver -> Word64
+  part1 :: solver -> Int
+  part2 :: solver -> Int
 
-newtype Day1 = Day1 [Word64]
+newtype Day1 = Day1 [Int]
+
+countIncreasing :: Ord a => [a] -> Int
+countIncreasing xs@(_:ys) = length $ filter (uncurry (<)) $ zip xs ys
 
 instance Solver Day1 where
   setup = Day1 . map (read . unpack) . lines
-  part1 (Day1 xs) = head $ [x * y | x <- xs, y <- xs, x + y == 2020]
-  part2 (Day1 xs) = head $ [x * y * z | x <- xs, y <- xs, z <- xs, x + y + z == 2020]
+  part1 (Day1 xs) = countIncreasing xs
+  part2 (Day1 xs@(_:ys@(_:zs))) = countIncreasing $ zipWith3 (((+) .) . (+)) xs ys zs
