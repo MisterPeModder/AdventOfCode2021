a9845dba2690118131cb3ea9830f6bce70a892d2
Yanis Guaye <y.guaye@ganacos.com>
2021-12-06 10:17:28 +0100

    Day 6, part 1

--- src/AOC/Solutions/Day06.hs
+++ src/AOC/Solutions/Day06.hs
@@ -1,19 +1,26 @@
 module AOC.Solutions.Day06 where
 
 import AOC.Solver (Solution, solution)
-import Data.Text (Text, lines, unpack)
+import Data.Text (Text, lines, split, unpack)
+import Debug.Trace (trace)
 import Prelude hiding (lines)
 
-newtype Day06 = Day06 ()
+newtype Day06 = Day06 [Int]
 
 day06 :: Solution
 day06 = solution setup part1 part2
 
 setup :: Text -> Day06
-setup = error "NYI"
+setup = Day06 . map (read . unpack) . split (== ',')
 
 part1 :: Day06 -> Int
-part1 = error "NYI"
+part1 (Day06 xs) = length $ iterate fishCycle xs !! 80
 
 part2 :: Day06 -> Int
-part2 = error "NYI"
+part2 (Day06 xs) = length $ iterate fishCycle xs !! 256
+
+fishCycle :: [Int] -> [Int]
+fishCycle = concatMap fish
+  where
+    fish 0 = [6, 8]
+    fish x = [x - 1]
