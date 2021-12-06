cac06b6a9e253df3fb1cedb47f8530ceb39fab56
Yanis Guaye <yanis.guaye@epitech.eu>
2021-12-06 23:01:37 +0100

    Day 6, part 2

--- src/AOC/Solutions/Day06.hs
+++ src/AOC/Solutions/Day06.hs
@@ -1,11 +1,11 @@
-module AOC.Solutions.Day06 where
+module AOC.Solutions.Day06 (day06) where
 
 import AOC.Solver (Solution, solution)
-import Data.Text (Text, lines, split, unpack)
-import Debug.Trace (trace)
-import Prelude hiding (lines)
+import Data.Text (Text, split, unpack)
 
-newtype Day06 = Day06 [Int]
+newtype Day06 = Day06 { initialFishes :: [Int] }
+
+type FishCountPerDay = (Int, Int, Int, Int, Int, Int, Int, Int, Int)
 
 day06 :: Solution
 day06 = solution setup part1 part2
@@ -14,13 +14,28 @@ setup :: Text -> Day06
 setup = Day06 . map (read . unpack) . split (== ',')
 
 part1 :: Day06 -> Int
-part1 (Day06 xs) = length $ iterate fishCycle xs !! 80
+part1 = countAtDay 80 . initialFishes
 
 part2 :: Day06 -> Int
-part2 (Day06 xs) = length $ iterate fishCycle xs !! 256
+part2 = countAtDay 256 . initialFishes
 
-fishCycle :: [Int] -> [Int]
-fishCycle = concatMap fish
-  where
-    fish 0 = [6, 8]
-    fish x = [x - 1]
+countAtDay :: Int -> [Int] -> Int
+countAtDay day = sum . map (fcount . (!! day) . iterate fcycle . initDayCount)
+
+initDayCount :: Int -> FishCountPerDay
+initDayCount 0 = (1, 0, 0, 0, 0, 0, 0, 0, 0)
+initDayCount 1 = (0, 1, 0, 0, 0, 0, 0, 0, 0)
+initDayCount 2 = (0, 0, 1, 0, 0, 0, 0, 0, 0)
+initDayCount 3 = (0, 0, 0, 1, 0, 0, 0, 0, 0)
+initDayCount 4 = (0, 0, 0, 0, 1, 0, 0, 0, 0)
+initDayCount 5 = (0, 0, 0, 0, 0, 1, 0, 0, 0)
+initDayCount 6 = (0, 0, 0, 0, 0, 0, 1, 0, 0)
+initDayCount 7 = (0, 0, 0, 0, 0, 0, 0, 1, 0)
+initDayCount 8 = (0, 0, 0, 0, 0, 0, 0, 0, 1)
+initDayCount x = error $ "day count cannot exceed 8, got " ++ show x
+
+fcycle :: FishCountPerDay -> FishCountPerDay
+fcycle (c0, c1, c2, c3, c4, c5, c6, c7, c8) = (c1, c2, c3, c4, c5, c6, c7 + c0, c8, c0)
+
+fcount :: FishCountPerDay -> Int
+fcount (c0, c1, c2, c3, c4, c5, c6, c7, c8) = c0 + c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8
