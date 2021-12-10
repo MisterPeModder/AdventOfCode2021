85418f2d8adc47135d97fac325924354111f6fa1
Yanis Guaye <y.guaye@ganacos.com>
2021-12-10 10:21:38 +0100

    Day 10 improvements

--- src/AOC/Solutions/Day10.hs
+++ src/AOC/Solutions/Day10.hs
@@ -2,8 +2,8 @@ module AOC.Solutions.Day10 (day10) where
 
 import AOC.Solver (Solution, solution)
 import Data.List (sort)
+import Data.Maybe (mapMaybe)
 import Data.Text (Text, lines, unpack)
-import Debug.Trace (trace)
 import Prelude hiding (lines)
 
 newtype Day10 = Day10 {inputLines :: [String]}
@@ -18,32 +18,17 @@ setup = Day10 . map unpack . lines
 
 part1 :: Day10 -> Int
 part1 = sum . map (maybe 0 (pointsFor . fst) . findIllegal []) . inputLines
-
-toComplete :: Day10 -> [ToComplete]
-toComplete = foldl addJust [] . map (fmap snd . findIllegal []) . inputLines
   where
-    addJust xs (Just x) = x : xs
-    addJust xs _ = xs
-
-calcScore :: Int -> ToComplete -> Int
-calcScore s ('(' : xs) = calcScore (s * 5 + 1) xs
-calcScore s ('[' : xs) = calcScore (s * 5 + 2) xs
-calcScore s ('{' : xs) = calcScore (s * 5 + 3) xs
-calcScore s ('<' : xs) = calcScore (s * 5 + 4) xs
-calcScore s [] = s
-calcScore s xs = trace ("ZERO: s=" ++ show s ++ ", xs=" ++ show xs) 0
+    pointsFor ')' = 3
+    pointsFor ']' = 57
+    pointsFor '}' = 1197
+    pointsFor '>' = 25137
+    pointsFor _ = 0
 
 part2 :: Day10 -> Int
-part2 = getMiddle . sort . map (calcScore 0) . filter (not . null) . toComplete
+part2 = getMiddle . sort . map (calcScore 0) . filter (not . null) . mapMaybe (fmap snd . findIllegal []) . inputLines
   where
-    getMiddle xs = trace ("GET_MIDDLE: xs=" ++ show xs) xs !! (length xs `div` 2)
-
-pointsFor :: Char -> Int
-pointsFor ')' = 3
-pointsFor ']' = 57
-pointsFor '}' = 1197
-pointsFor '>' = 25137
-pointsFor _ = 0
+    getMiddle xs = length xs `div` 2
 
 findIllegal :: ToComplete -> String -> Maybe (Char, ToComplete)
 findIllegal ('(' : stack) (')' : line) = findIllegal stack line
@@ -59,3 +44,13 @@ findIllegal [] "" = Nothing
 findIllegal _ (c : _) = Just (c, [])
 -- incomplete line
 findIllegal stack _ = Just ('?', stack)
+
+calcScore :: Int -> ToComplete -> Int
+calcScore s [] = s
+calcScore s (x : xs) = calcScore (s * 5 + baseScore x) xs
+  where
+    baseScore '(' = 1
+    baseScore '[' = 2
+    baseScore '{' = 3
+    baseScore '<' = 4
+    baseScore c = error $ "Incorrect opening character " ++ show c
